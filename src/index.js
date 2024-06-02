fe690a75d6bad877e9698adaaa8d2d728bb87416
enryq3 <mah.enryq3@gmail.com>
2024-06-02 11:57:38 -0300

    Conclusão do Projeto

--- src/index.js
+++ src/index.js
@@ -1,2 +1,15 @@
- let nome = "Marcelo"
- console.log(nome) 
+let heroi = "Tauros"
+let xp = 1500
+let nivel = " "
+
+if (xp <= 1000) {
+  nivel = "Ferro"
+} else if((xp >= 1001) && (xp <= 2000)) {
+  nivel = "Bronze"
+} else if((xp >= 2001) && (xp <= 5000)) {
+  nivel = "Prata"
+} else {
+  nivel = "Ouro"
+}
+
+console.log("O heroi " + heroi + " tem " + xp + " de xp" + " ele está no nível " + nivel)
